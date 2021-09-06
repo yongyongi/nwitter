1ff3141a1c103697c012a629c6dee9c112028612
Yongyong <yongs8425@gmail.com>
2021-09-06 23:54:49 +0900

    Add Real-time tweet

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,14 @@
 import { db } from "fbase";
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  onSnapshot,
+  orderBy,
+  quer,
+} from "firebase/firestore";
 
 import React, { useEffect, useState } from "react";
 
@@ -7,12 +16,30 @@ const Home = () => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
 
-  useEffect(async () => {
-    const querySnapshot = await getDocs(collection(db, "nweets"));
-    querySnapshot.forEach((doc) => {
-      const nweetObject = { ...doc.data(), id: doc.id };
-      setNweets((prev) => [nweetObject, ...prev]);
+  useEffect(() => {
+    // const querySnapshot = await getDocs(collection(db, "nweets"));
+    // querySnapshot.forEach((doc) => {
+    //   const nweetObject = { ...doc.data(), id: doc.id };
+    //   setNweets((prev) => [nweetObject, ...prev]);
+    // }); // 실시간으로 바뀌지 않음, 실시간으로 바뀌려면 onSnapshot 사용 해야함.
+    const q = query(collection(db, "nweets"), orderBy("createdAt", "desc"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      //   querySnapshot.forEach((doc) => {
+      //     const nweetObject = { ...doc.data(), id: doc.id };
+      //     setNweets((prev) => [nweetObject, ...prev]);
+      //   }); // 기존의 데이터가 남아 있는채로 중첩되어 랜더링됌, 실시간으로 바뀌긴함
+      const newArray = querySnapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+      setNweets(newArray);
     });
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
   const onChange = (e) => {
     const {
